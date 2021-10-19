33691bab7ad5e06fff9206797f84499b48e18d14
sam002696 <sadmansakib221@gmail.com>
2021-10-19 18:36:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import About from './Components/Paths/About/About';
+import Blog from './Components/Paths/Blog/Blog';
+import Doctor from './Components/Paths/Doctor/Doctor';
+import Home from './Components/Paths/Home/Home';
+import Login from './Components/Paths/Login/Login';
+import NotFound from './Components/Paths/NotFound/NotFound';
+import PrivateRoute from './Components/Paths/PrivateRoute/PrivateRoute';
+import Service from './Components/Paths/Service/Service';
+import ServiceDetail from './Components/Paths/ServiceDetails/ServiceDetail';
+import AuthProvider from './ContextApi/AuthProvider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <PrivateRoute path="/service">
+            <Service></Service>
+          </PrivateRoute>
+          <Route path="/doctor">
+            <Doctor></Doctor>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <PrivateRoute path="/serviceDetail/:serviceID">
+            <ServiceDetail></ServiceDetail>
+          </PrivateRoute>
+          <PrivateRoute path="/blog">
+            <Blog></Blog>
+          </PrivateRoute>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
+    </AuthProvider>
   );
 }
 
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFOuqXiMHqwlN0-XzyIL80RHYQoxF79as",
+    authDomain: "healthcare-web-93407.firebaseapp.com",
+    projectId: "healthcare-web-93407",
+    storageBucket: "healthcare-web-93407.appspot.com",
+    messagingSenderId: "1005582368956",
+    appId: "1:1005582368956:web:b771cb0933f7b831afd0b9"
+};
+export default firebaseConfig;
